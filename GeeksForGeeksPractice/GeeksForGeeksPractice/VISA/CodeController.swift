d45dd1c0c0053c777acf4bbe34a718ec29a0bf2b
Dipesh <dipesh.pokhrel.ext@zeiss.com>
2021-05-28 07:33:24 +0530

    Find out the maximum number of guest present in a particular period of time, Sorting is manual

--- GeeksForGeeksPractice/GeeksForGeeksPractice/VISA/CodeController.swift
+++ GeeksForGeeksPractice/GeeksForGeeksPractice/VISA/CodeController.swift
@@ -8,22 +8,46 @@
 import UIKit
 
 class CodeController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        //sort two arrays
+        //both the arrays are sort
+        var entryTime =  [1,2,5,5,10]
+        var exitTime  = [4,5,5,9,12]
+        let mergedArray : [Int] = [1,2,4,5,5,5,5,9,10,12]
+        
+        var entryExitArray  : [Int] = []
+        for counter in 0..<mergedArray.count {
+            var obj  = mergedArray[counter]
+            if entryTime.contains(obj) {
+                let index = entryTime.firstIndex(of: obj)!
+                entryExitArray.append(0)
+                entryTime[index] = -1
+            }
+            else if exitTime.contains(obj) {
+                let index = exitTime.firstIndex(of: obj)!
+                entryExitArray.append(1)
+                exitTime[index] = -1
+            }
+        }
+        var numberOfEntry = 0
+        var numberOfExit = 0
+        var maximum = 0
+        for counter in 0..<entryExitArray.count {
+            numberOfEntry = 0
+            numberOfExit = 0
+            for value in 0...counter {
+                let key = entryExitArray [value]
+                if key == 0 {
+                    numberOfEntry = numberOfEntry + 1
+                }
+                if key == 1 {
+                    numberOfExit = numberOfExit + 1
+                }
+            }
+            let values =  numberOfEntry - numberOfExit
+            maximum = max(maximum, values)
+        }
+        print(maximum)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
