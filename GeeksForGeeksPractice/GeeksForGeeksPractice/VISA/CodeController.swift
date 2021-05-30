d4dc04dd1208ab0316f9f1119298696d43250730
Dipesh <dipesh.pokhrel.ext@zeiss.com>
2021-05-30 08:31:22 +0530

    kadanes algorithm to find maximum of subarray

--- GeeksForGeeksPractice/GeeksForGeeksPractice/VISA/CodeController.swift
+++ GeeksForGeeksPractice/GeeksForGeeksPractice/VISA/CodeController.swift
@@ -6,11 +6,22 @@
 //
 
 import UIKit
-
+//kadanes Algorithm to find maximum
 class CodeController: UIViewController {
-    
+    let sampleArray = [2,-3,-4,6,7,8,-19,2,3,4,5,6,7,8,9]
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        var maximumSum = 0
+        var currentSum = 0
+        for i in 0..<sampleArray.count {
+            currentSum = currentSum + sampleArray[i]
+            if currentSum < 0 {
+                currentSum = 0
+            }
+            if currentSum > maximumSum {
+                maximumSum = currentSum
+            }
+        }
+        print(maximumSum)
     }
 }
