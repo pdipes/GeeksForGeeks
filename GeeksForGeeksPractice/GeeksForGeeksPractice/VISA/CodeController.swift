7c8421a40b08bb5dcdd568dbcae17120e0ca28f8
Dipesh <dipesh.pokhrel.ext@zeiss.com>
2021-05-30 08:12:04 +0530

    Brute force of maximum subarray

--- GeeksForGeeksPractice/GeeksForGeeksPractice/VISA/CodeController.swift
+++ GeeksForGeeksPractice/GeeksForGeeksPractice/VISA/CodeController.swift
@@ -6,11 +6,25 @@
 //
 
 import UIKit
-
+//Get maximum sum of subarray from an array
 class CodeController: UIViewController {
-    
+    let sample = [2,-3,4,5]
+    var sum = 0
+    var maximumSum = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        //get all substrings
+        for i in 0..<sample.count {
+            for j in i..<sample.count {
+                for k in i...j {
+                    sum = sum + sample[k]
+                    maximumSum =  max(maximumSum, sum)
+               
+                }
+                sum = 0
+             
+            }
+        }
+        print(maximumSum)
     }
 }
