13a0f52ac9fd9aa3f9abc1811abd52cc244b7b6f
Dipesh <dipesh.pokhrel.ext@zeiss.com>
2021-06-03 20:55:00 +0530

    Find matching sequence of data for a target sum

--- GeeksForGeeksPractice/GeeksForGeeksPractice/VISA/CodeController.swift
+++ GeeksForGeeksPractice/GeeksForGeeksPractice/VISA/CodeController.swift
@@ -8,9 +8,29 @@
 import UIKit
 
 class CodeController: UIViewController {
-    
+    let inputs = [1,4,20,3,10,5] //given all the array non negative
+    let targetSum = 28
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        var total = 0
+        for input in inputs {
+            total = total + input
+        }
+        searchSubArray(with : 0, endingIndex: inputs.count - 1, and: total)
+    }
+    func searchSubArray(with startingIndex : Int, endingIndex : Int , and currentSum : Int) {
+        if  startingIndex > endingIndex{
+            print("No match found!")
+            return
+        }
+        else if currentSum == targetSum {
+            print("startingIndex \(startingIndex)")
+            print("endingIndex \(endingIndex)")
+        }else {
+            var temporary = currentSum - inputs[startingIndex]
+            searchSubArray(with: startingIndex + 1, endingIndex: endingIndex , and: temporary)
+            temporary = currentSum - inputs[endingIndex]
+            searchSubArray(with: startingIndex, endingIndex: endingIndex - 1, and: temporary)
+        }
     }
 }
